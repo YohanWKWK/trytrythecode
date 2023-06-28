f45b6ef331119d49ff80c6afef79b5da76a8c263
Vian <marcellinusyovian@gmail.com>
2023-06-29 01:02:42 +0700

    testing

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('full_name');
-            $table->string('email');
-            $table->string('kota_Kabupaten');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('kota_kabupaten');
             $table->text('alamat_lengkap');
+            $table->string('photo')->nullable();
             $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
